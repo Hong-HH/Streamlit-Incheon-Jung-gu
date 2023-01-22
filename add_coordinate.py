ad8fcb6da5a5d38ac50abfae324b2209bf9bc01c
Hong-HH <a1guskn7@gmail.com>
2023-01-22 21:41:52 +0900

    add_coordinate_complete

--- add_coordinate.py
+++ add_coordinate.py
@@ -22,70 +22,74 @@ def get_address(keyword) :
     location_list = response['documents']
     print (len(location_list))
 
-    temp_location = []
-    temp_x = []
-    temp_y = []
+    # 리스트 일시 api가 같은 주소 2번 리턴하였을 때 중복해서 기록됨을 확인, set으로 바꿈
+    temp_location = set()
+    temp_x = set()
+    temp_y = set()
 
     for location in location_list :
         road_address_name = location['road_address_name']
         print(road_address_name)
         if '인천 중구' in road_address_name :
-            temp_location.append(road_address_name)
-            temp_x.append({location['x']})
-            temp_y.append({location['y']})
+            print("등록중~")
+            temp_location.add(road_address_name)
+            temp_x.add(location['x'])
+            temp_y.add(location['y'])
 
 
     if len(temp_location) == 1 :
-        return [temp_location[0], temp_x[0], temp_y[0]]
+        return [list(temp_location)[0], list(temp_x)[0], list(temp_y)[0]]
     else :
         return ['', '', '']
 
 
-test_location = get_address("인천 중구 연안의원")
+# test_location = get_address("인천 중구 연안의원")
 
-print(test_location)
-print(len(test_location))
-print(test_location[0])
-print(test_location[1])
-print(test_location[2])
-print(type(test_location[1]))
+# print(test_location)
+# print(len(test_location))
+# print(test_location[0])
+# print(test_location[1])
+# print(test_location[2])
+# print(type(test_location[1]))
 
-print(list(test_location[2]))
-print(type(list(test_location[2])))
-print(list(test_location[2])[0])
-
-
-# st.title("가까운 병원 찾기")
-
-# df = pd.read_csv('data/hospital.csv')
-
-# st.dataframe(df)
-
-# # 행의 수를 추출
-# num_lows = df.shape[0]
-
-# # 행으로 넣어줄 빈 리스트 생성
-# location = []
-# x_coordinate = []
-# y_coordinate = []
-
-# # 행의 수와 인덱스와 같은 리스트를 생성해 반복
-# for i in list(np.arange(num_lows)) :
-#     # 병원이름 추출
-#     keyword = df.iloc[i, 1] 
-#     print(keyword)
-#     location_list = []
-#     location_list = get_address(keyword)
-
-#     location.append(location_list[0])
-#     x_coordinate.append(location_list[1])
-#     y_coordinate.append(location_list[2])
+# print(list(test_location[2]))
+# print(type(list(test_location[2])))
+# print(list(test_location[2])[0])
 
 
-# df['location'] = location 
-# df['x_coordinate'] = x_coordinate 
-# df['y_coordinate'] = y_coordinate 
+st.title("병원 주소와 좌표 찾기")
+
+df = pd.read_csv('data/hospital.csv')
+
+st.dataframe(df)
+
+# 행의 수를 추출
+num_lows = df.shape[0]
+
+# 행으로 넣어줄 빈 리스트 생성
+location = []
+x_coordinate = []
+y_coordinate = []
+
+# 행의 수와 인덱스와 같은 리스트를 생성해 반복
+for i in list(np.arange(num_lows)) :
+    # 병원이름 추출
+    keyword = df.iloc[i, 1] 
+    print(keyword)
+    location_list = []
+    location_list = get_address(keyword)
+
+    location.append(location_list[0])
+    x_coordinate.append(location_list[1])
+    y_coordinate.append(location_list[2])
 
 
-# st.dataframe(df)
+df['location'] = location 
+df['x_coordinate'] = x_coordinate 
+df['y_coordinate'] = y_coordinate 
+
+
+st.dataframe(df)
+
+df.to_csv('data/hospital_location.csv')
 
